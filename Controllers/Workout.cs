8bbda29c37f740fdd29a3ea5092e6626a6b3bf26
rafbar34 <rafbar34@gmail.com>
2024-11-28 22:16:46 +0100

    added workout get

---
+++ Controllers/Workout.cs
@@ -0,0 +1,39 @@
+﻿using FYR_api.Model;
+using FYR_api.NewFolder;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYR_api.Controllers
+{
+    public class Workout : ControllerBase
+    {
+        private readonly ConnectDBService? _connectDb;
+        
+            public Workout(ConnectDBService? connectDb)
+            { 
+            _connectDb = connectDb;
+            }
+        
+
+        [HttpGet]
+        [Route("/workout/{id}")]
+        public async Task<ActionResult> GetWorkouts(Guid id)
+        {
+            var getExpection = new GetExceptionHandler();
+            var supabaseClient = _connectDb.SupabaseClient;
+
+
+            var result = await getExpection.ExpectionHandler(async () =>
+            {
+                var res = await supabaseClient.From<WorkoutModel>().Where((x) => x.UserId == id).Get();
+                return res.Models;
+            });
+            return Ok(result);
+
+        }
+    }
+}
---
+++ Model/WorkoutModel.cs
@@ -0,0 +1,31 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYR_api.Model
+{
+    [Table("workout")]
+    class WorkoutModel : BaseModel
+    {
+    
+        [Column("id")]
+        public int Id { get; set; }
+    
+        [Column("user_id")]
+        public Guid UserId { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("workout_info")]
+        public object WorkoutInfo { get; set; }
+
+        [Column("duration")]
+        public DateTime Duration { get; set; }
+
+    }
+}
